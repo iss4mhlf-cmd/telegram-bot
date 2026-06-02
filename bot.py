16ec947b7c54b151f64dfdbaa4cb410085bd8022
iss4mhlf-cmd <iss4mhlf@gmail.com>
2026-06-02 21:32:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
+import os
 TOKEN = os.getenv("8682987871:AAGj-SlBB1at2DiOeGqUMPbfiedME8hIR8c")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
